0776035994a47388535d004f601a3736de5e34d8
KeerthanaMerugu3 <98105702+KeerthanaMerugu3@users.noreply.github.com>
2022-08-24 12:09:00 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*******************************************************************
+* Java Program Name
+********************************
+* Keerthana Merugu
+* 8/24/22
+* CMSC 255 004
+*************************/
+
+public class Hello {
+	public static void main (String[] args) {
+		System.out.println("<Your Name>");
+		System.out.println("<Java Program Name>");
+		System.out.println("<Program/Project Description>");
+		System.out.println("<Date Created>");
+		System.out.println("<Course Number and Section Number>");
+		System.out.println();
+	}
+}
